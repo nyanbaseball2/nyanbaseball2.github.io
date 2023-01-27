c0bf2f5b84b896aea56823571e89ae2638613ce2
Nonwhiskerscat <nonwhiskerscat@gmail.com>
2023-01-27 14:04:54 +0900

    oo

--- js/alertCustom.js
+++ js/alertCustom.js
@@ -81,7 +81,7 @@ function alertResult(cat, escore, earr, etry) {
             rstTitle.style.color='#ff0000';
         })
         rstH4.innerHTML=`Failed`;
-        rstImg.src='../img/alert/catfailed.png';
+        rstImg.src='./img/alert/catfailed.png';
 
         if(lolly==2) {
             rstNtc.innerHTML=`중도 포기로 인해 <br>공격에 실패하였습니다.`;
@@ -135,37 +135,37 @@ function alertCommon(cat, meow, nyan) {
 
     if(lolly==1) {
         rstH4.innerHTML='Game Start!';
-        rstImg.src='../img/alert/cat.png';
+        rstImg.src='./img/alert/cat.png';
         rstNtc.innerHTML=`게임을 시작합니다.`;
     }
 
     if(lolly==2) {
         rstH4.innerHTML=`${outArr[myOut-1]} Out`;
-        rstImg.src='../img/alert/catout.png';
+        rstImg.src='./img/alert/catout.png';
         rstNtc.innerHTML=`일치하는 숫자가 없어 <br>아웃을 당하셨습니다.`;
     }
 
     if(lolly==3) {
         rstH4.innerHTML=`${myStrike} Strike`;
-        rstImg.src='../img/alert/cathappy.png';
+        rstImg.src='./img/alert/cathappy.png';
         rstNtc.innerHTML=`${strArr[myStrike-2]} 개의 스트를 잡았습니다.! <br> 마지막까지 파이팅!`;
     }
 
     if(lolly==4) {
         rstH4.innerHTML=`Warning`;
-        rstImg.src='../img/alert/catout.png';
+        rstImg.src='./img/alert/catout.png';
         rstNtc.innerHTML=`점수가 부족하여 <br> 아이템 구매를 할 수 없습니다..`;
     }
 
     if(lolly==5) {
         rstH4.innerHTML=`Warning`;
-        rstImg.src='../img/alert/catout.png';
+        rstImg.src='./img/alert/catout.png';
         rstNtc.innerHTML=`공격 숫자가 4자리 이상인<BR>경우에만 사용 가능합니다.`;
     }
 
     if(lolly==6) {
         rstH4.innerHTML=`Warning`;
-        rstImg.src='../img/alert/catout.png';
+        rstImg.src='./img/alert/catout.png';
         rstNtc.innerHTML=`이미 판매가 완료된 힌트입니다.`;
     }
 
